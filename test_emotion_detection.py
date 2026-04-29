7f90cd05809660282c85707e6b47c0154228d9c1
arshadrazvi <arshad.razvi@gmail.com>
2026-04-29 15:28:20 -0400

    task-5

---
+++ test_emotion_detection.py
@@ -0,0 +1,38 @@
+'''
+    Test cases for emotion_detection.py
+'''
+
+import unittest
+from emotion_detection import emotion_detector
+
+class TestEmotionDetectionr(unittest.TestCase):
+    '''
+        Test cases class designation for unit test
+    '''
+
+    def test_emotion_detection(self):
+        '''
+            function for unit testing against
+            all emotions
+        '''
+        # Test case for positive sentiment
+        result_1 = emotion_detector('I am glad this happened')
+        self.assertEqual(result_1[1]['label'], 'joy')
+
+        # Test case for negative sentiment
+        result_2 = emotion_detector('I am really mad about this')
+        self.assertEqual(result_2[1]['label'], 'anger')
+
+        # Test case for neutral sentiment
+        result_3 = emotion_detector('I feel disgusted just hearing about this')
+        self.assertEqual(result_3[1]['label'], 'disgust')
+
+        # Test case for neutral sentiment
+        result_4 = emotion_detector('I am so sad about this')
+        self.assertEqual(result_4[1]['label'], 'sadness')
+
+        # Test case for neutral sentiment
+        result_5 = emotion_detector('I am really afraid that this will happen')
+        self.assertEqual(result_5[1]['label'], 'fear')
+
+unittest.main()
